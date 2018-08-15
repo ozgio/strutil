9959cc75763248c19dc8d0c7a281d54cb2bca8bf
Ozgur Erdem <gudubeth@gmail.com>
2018-08-15 17:40:19 +0300

    Fix and add tests for Box

--- box.go
+++ box.go
@@ -27,12 +27,23 @@ var DefaultBox9Slice Box9Slice = Box9Slice{
 	TopLeft:     "+",
 }
 
+var SimpleBox9Slice Box9Slice = Box9Slice{
+	Top:         "-",
+	TopRight:    "+",
+	Right:       "|",
+	BottomRight: "+",
+	Bottom:      "-",
+	BottomLeft:  "+",
+	Left:        "|",
+	TopLeft:     "+",
+}
+
 func CustomBox(content string, width int, align string, chars Box9Slice) (string, error) {
 	var buff strings.Builder
 
-	var topInsideWidth = width - len(chars.TopLeft) - len(chars.TopRight)
-	var middleInsideWidth = width - len(chars.Left) - len(chars.Right)
-	var bottomInsideWidth = width - len(chars.BottomLeft) - len(chars.BottomRight)
+	var topInsideWidth = width - UTF8Len(chars.TopLeft) - UTF8Len(chars.TopRight)
+	var middleInsideWidth = width - UTF8Len(chars.Left) - UTF8Len(chars.Right)
+	var bottomInsideWidth = width - UTF8Len(chars.BottomLeft) - UTF8Len(chars.BottomRight)
 	if topInsideWidth < 1 || middleInsideWidth < 1 || bottomInsideWidth < 1 {
 		return "", errors.New("there is not enough width")
 	}
@@ -40,10 +51,19 @@ func CustomBox(content string, width int, align string, chars Box9Slice) (string
 	buff.WriteString(chars.TopLeft)
 	buff.WriteString(strings.Repeat(chars.Top, topInsideWidth))
 	buff.WriteString(chars.TopRight)
+	buff.WriteString("\n")
 
 	buff.WriteString(MapLines(content, func(line string) string {
-		return chars.Left + Align(line, align, width) + chars.Right
+		line = Align(line, align, middleInsideWidth)
+		if align == AlignTypeLeft {
+			line = PadRight(line, middleInsideWidth, " ")
+		}
+		if line == "" {
+			line = strings.Repeat(" ", middleInsideWidth)
+		}
+		return chars.Left + line + chars.Right
 	}))
+	buff.WriteString("\n")
 
 	buff.WriteString(chars.BottomLeft)
 	buff.WriteString(strings.Repeat(chars.Bottom, bottomInsideWidth))
--- box_test.go
+++ box_test.go
@@ -1,1 +1,25 @@
 package strutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBox(t *testing.T) {
+	tests := []struct {
+		input    string
+		width    int
+		align    string
+		expected string
+	}{
+		{"Hello World", 20, AlignTypeCenter, "+------------------+\n|   Hello World    |\n+------------------+"},
+		{"\nHello World\n", 20, AlignTypeCenter, "+------------------+\n|                  |\n|   Hello World    |\n|                  |\n+------------------+"},
+		{"résumé", 10, AlignTypeLeft, "+--------+\n|résumé  |\n+--------+"},
+	}
+
+	for i, test := range tests {
+		output, _ := Box(test.input, test.width, test.align)
+		assert.Equalf(t, test.expected, output, "Test case %d is not successful\n", i)
+	}
+}
