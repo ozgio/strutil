d718b34bcdaf52a6b48c55a69c5b280233832016
Ozgur Erdem <gudubeth@gmail.com>
2019-10-07 01:08:58 +0200

    Add SplitAndMap function

--- map.go
+++ map.go
@@ -6,9 +6,14 @@ import "strings"
 // It splits the string by new line "\n" and runs the fn for every line and
 // returns the new string by combining these lines with "\n"
 func MapLines(str string, fn func(string) string) string {
-	arr := strings.Split(str, "\n")
+	return SplitAndMap(str, "\n", fn)
+}
+
+//SplitAndMap splits the string and runs the function fn on every part
+func SplitAndMap(str string, split string, fn func(string) string) string {
+	arr := strings.Split(str, split)
 	for i := 0; i < len(arr); i++ {
 		arr[i] = fn(arr[i])
 	}
-	return strings.Join(arr, "\n")
+	return strings.Join(arr, split)
 }
